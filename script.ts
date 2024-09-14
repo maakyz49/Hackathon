6724941e57b0f2ebd25cbaf8019aed8d66b0a967
maakyz49 <maakyz4974@gmail.com>
2024-09-14 09:11:20 +0500

    Add files via upload

--- script.ts
+++ script.ts
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Get elements by ID
     const toggleExperienceButton = document.getElementById('toggleExperience') as HTMLButtonElement;
     const experienceSection = document.getElementById('experienceSection') as HTMLDivElement;
+
+    const toggleEducationButton = document.getElementById('toggleEducation') as HTMLButtonElement;
+    const educationSection = document.getElementById('educationSection') as HTMLDivElement;
 
     const toggleSkillsButton = document.getElementById('toggleSkills') as HTMLButtonElement;
     const skillsSection = document.getElementById('skillsSection') as HTMLDivElement;
@@ -8,14 +12,21 @@ document.addEventListener("DOMContentLoaded", () => {
     if (toggleExperienceButton && experienceSection) {
         toggleExperienceButton.addEventListener('click', () => {
             experienceSection.classList.toggle('d-none');
-            toggleExperienceButton.textContent = experienceSection.classList.contains('d-none') ? 'Show' : 'Hide';
+            toggleExperienceButton.textContent = experienceSection.classList.contains('d-none') ? 'Show Experience' : 'Hide Experience';
         });
     }
 
     if (toggleSkillsButton && skillsSection) {
         toggleSkillsButton.addEventListener('click', () => {
             skillsSection.classList.toggle('d-none');
-            toggleSkillsButton.textContent = skillsSection.classList.contains('d-none') ? 'Show' : 'Hide';
+            toggleSkillsButton.textContent = skillsSection.classList.contains('d-none') ? 'Show Skills' : 'Hide Skills';
+        });
+    }
+
+    if (toggleEducationButton && educationSection) {
+        toggleEducationButton.addEventListener('click', () => {
+            educationSection.classList.toggle('d-none');
+            toggleEducationButton.textContent = educationSection.classList.contains('d-none') ? 'Show Education' : 'Hide Education';
         });
     }
 });
